261e21a019da25de5d04028d64c9cc565be6e2af
tartamar <martin.tartarelli@gmail.com>
2014-07-27 02:38:56 -0300

    First Commit
    Upload
    First Commit

---
+++ chusma/Form_Main.Designer.vb
@@ -0,0 +1,122 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form_Main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form_Main))
+        Me.btnPath = New System.Windows.Forms.Button
+        Me.txtPath = New System.Windows.Forms.TextBox
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox
+        Me.chkActivar = New System.Windows.Forms.CheckBox
+        Me.ListView1 = New System.Windows.Forms.ListView
+        Me.FileSystemWatcher1 = New System.IO.FileSystemWatcher
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.FileSystemWatcher1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnPath
+        '
+        Me.btnPath.Location = New System.Drawing.Point(487, 17)
+        Me.btnPath.Name = "btnPath"
+        Me.btnPath.Size = New System.Drawing.Size(75, 23)
+        Me.btnPath.TabIndex = 0
+        Me.btnPath.Text = "Select"
+        Me.btnPath.UseVisualStyleBackColor = True
+        '
+        'txtPath
+        '
+        Me.txtPath.Location = New System.Drawing.Point(6, 19)
+        Me.txtPath.Name = "txtPath"
+        Me.txtPath.Size = New System.Drawing.Size(475, 20)
+        Me.txtPath.TabIndex = 1
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.chkActivar)
+        Me.GroupBox1.Controls.Add(Me.btnPath)
+        Me.GroupBox1.Controls.Add(Me.txtPath)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(719, 52)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "PATH File or Directory"
+        '
+        'chkActivar
+        '
+        Me.chkActivar.AutoSize = True
+        Me.chkActivar.Location = New System.Drawing.Point(628, 21)
+        Me.chkActivar.Name = "chkActivar"
+        Me.chkActivar.Size = New System.Drawing.Size(79, 17)
+        Me.chkActivar.TabIndex = 4
+        Me.chkActivar.Text = "Notification"
+        Me.chkActivar.UseVisualStyleBackColor = True
+        '
+        'ListView1
+        '
+        Me.ListView1.Location = New System.Drawing.Point(12, 70)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(719, 180)
+        Me.ListView1.TabIndex = 3
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        '
+        'FileSystemWatcher1
+        '
+        Me.FileSystemWatcher1.EnableRaisingEvents = True
+        Me.FileSystemWatcher1.SynchronizingObject = Me
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(568, 17)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(54, 23)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Save"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form_Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(742, 262)
+        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form_Main"
+        Me.Text = "Chusma"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.FileSystemWatcher1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnPath As System.Windows.Forms.Button
+    Friend WithEvents txtPath As System.Windows.Forms.TextBox
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents chkActivar As System.Windows.Forms.CheckBox
+    Friend WithEvents ListView1 As System.Windows.Forms.ListView
+    Friend WithEvents FileSystemWatcher1 As System.IO.FileSystemWatcher
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+
+End Class
---
+++ chusma/Form_Main.vb
@@ -0,0 +1,157 @@
+﻿Option Explicit On
+Option Strict On
+
+Imports System.IO
+
+Public Class Form_Main
+
+    ' Object NotifyIcon - tray form
+    Private WithEvents mNotifyIcon As New NotifyIcon
+
+    Private Sub btnPath_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnPath.Click
+
+        'Open to select folder and files
+        Dim oFolderBrowser As New FolderBrowserDialog
+
+        With oFolderBrowser
+            .SelectedPath = ""
+            If .ShowDialog = Windows.Forms.DialogResult.OK Then
+                txtPath.Text = .SelectedPath
+                .Dispose()
+                cambiar_FSW(FileSystemWatcher1)
+            End If
+        End With
+
+    End Sub
+
+    Private Sub Form1_FormClosed(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosedEventArgs) Handles Me.FormClosed
+        mNotifyIcon.Dispose()
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        ' Started a FilseSystemWatcher properties
+        With FileSystemWatcher1
+            ' Include Directory to perform a monitoring
+            .IncludeSubdirectories = False
+            .EnableRaisingEvents = False
+            ' Monitor al files
+            .Filter = "*.*"
+            ' Filters: Create, Change, Read, Write
+            .NotifyFilter = NotifyFilters.CreationTime Or _
+                            NotifyFilters.Size Or _
+                            NotifyFilters.FileName Or _
+                            NotifyFilters.LastAccess
+
+            'MsgBox(.NotifyFilter)
+            chkActivar.Text = "Notification"
+        End With
+
+        ' Add columns (path, type and date)
+        With ListView1
+            .View = View.Details
+            .Columns.Add("Files ", 300)
+            .Columns.Add("Type of Modification", 120)
+            .Columns.Add("Date", 200)
+        End With
+
+        btnPath.Text = "Select"
+
+        ' NotifyIcon
+        With mNotifyIcon
+            .Icon = Me.Icon
+        End With
+
+    End Sub
+
+    Private Sub chkActivar_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles chkActivar.CheckedChanged
+        cambiar_FSW(FileSystemWatcher1)
+    End Sub
+
+    Sub cambiar_FSW(ByVal FSW As FileSystemWatcher)
+
+        Try
+            ' Check if directory exist
+            If Directory.Exists(txtPath.Text) = True Then
+                With FSW
+                    .Path = txtPath.Text
+                    ' Enable or disable FileSystemWatcher
+                    .EnableRaisingEvents = chkActivar.Checked
+                End With
+            End If
+            ' error
+        Catch sError As Exception
+            MsgBox(sError.Message, MsgBoxStyle.Critical)
+        End Try
+
+    End Sub
+
+    ' Add info to listview
+    Sub Notificar_Cambio(ByVal Path As String, ByVal type As String)
+
+        Dim oItem As New ListViewItem(Path)
+        With oItem
+            .SubItems.Add(type)
+            .SubItems.Add(Date.Now.ToString)
+            ListView1.Items.Add(oItem)
+        End With
+
+        With mNotifyIcon
+            .ShowBalloonTip(5000, type, Path, ToolTipIcon.Info)
+        End With
+    End Sub
+
+    ' Change in Folders or Files
+    Private Sub FileSystemWatcher1_Changed(ByVal sender As System.Object, ByVal e As System.IO.FileSystemEventArgs) Handles FileSystemWatcher1.Changed
+        Notificar_Cambio(e.Name, "Change by " & Environment.UserName)
+    End Sub
+
+    ' Create Files or Folders
+    Private Sub FileSystemWatcher1_Created(ByVal sender As Object, ByVal e As System.IO.FileSystemEventArgs) Handles FileSystemWatcher1.Created
+        Notificar_Cambio(e.Name, "Create by " & Environment.UserName)
+    End Sub
+
+    ' Delete Files or Folders
+    Private Sub FileSystemWatcher1_Deleted(ByVal sender As Object, ByVal e As System.IO.FileSystemEventArgs) Handles FileSystemWatcher1.Deleted
+        Notificar_Cambio(e.Name, "Delete by " & Environment.UserName)
+    End Sub
+
+    ' Error event
+    Private Sub FileSystemWatcher1_Error(ByVal sender As Object, ByVal e As System.IO.ErrorEventArgs) Handles FileSystemWatcher1.Error
+        MsgBox(e.GetException.ToString)
+    End Sub
+
+    ' NotifyIcon event
+    Private Sub mNotifyIcon_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles mNotifyIcon.Click
+        Me.Visible = True
+        Me.WindowState = FormWindowState.Normal
+    End Sub
+
+    ' Do enable the NotifyIcon
+    Private Sub Form1_Resize(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Resize
+
+        Select Case Me.WindowState
+            Case FormWindowState.Minimized
+                Me.Visible = False
+                mNotifyIcon.Visible = True
+        End Select
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        'Save content to text file
+        Dim directory As String = My.Application.Info.DirectoryPath
+        'Declare IO StreamWriter
+        Dim streamWriter As IO.StreamWriter = New IO.StreamWriter(directory & "chusma-output.txt")
+
+        'Save de result in a text file
+        For Each o As Object In ListView1.Items()
+            streamWriter.WriteLine(o.ToString())
+        Next
+        streamWriter.Flush()
+        streamWriter.Close()
+
+        MsgBox(directory & "chusma-output.txt - Saved")
+
+    End Sub
+End Class
